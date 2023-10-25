8320f1c46dde0ef2b2cba11c28a82d558bdacc06
Pietagorh <solal.hara@yahoo.com>
2023-10-25 14:46:04 +0200

    Add a second check + a few optimizations

--- VideoLoader.py
+++ VideoLoader.py
@@ -1,5 +1,6 @@
 import os.path as path
 import cv2
+from numpy import ndarray
 from pytube import YouTube
 from re import match
 import requests
@@ -14,22 +15,21 @@ class VideoLoader:
         self.cache = []
 
     def download(self) -> None:
-        if match("http(s)?://(www\.)?speedrun.com/fantasy_life/runs/[a-z0-9]{8}", self.link):
+        if match(r"http(s)?://(www\.)?speedrun.com/fantasy_life/runs/[a-z0-9]{8}", self.link):
             res = requests.get(self.link).text  # todo probably should clean that one day but for now it works
             res = res[res.index("<iframe class=\"block aspect-video w-full border-0\" src=\"") + 56:]
             res = res[:res.index("?")].replace("embed/", "watch?v=")
             self.link = res
 
-        if match("http(s)?://(www\.)?(youtube\.com/watch\?v=|youtu\.be/)[0-9A-Za-z_-]{11}", self.link):
+        if match(r"http(s)?://(www\.)?(youtube\.com/watch\?v=|youtu\.be/)[0-9A-Za-z_-]{11}", self.link):
             yt = YouTube(self.link)
             clean_title = "".join(filter(lambda char: match("[A-Za-z0-9_-]", char), yt.title)) + ".mp4"
             self.link = f"inputs/{clean_title}"
             if not path.exists(self.link) or input("Seems like this video is already on your computer. Would you still like to download it again? (Y/N)\n\t").upper() == "Y":
-                yt.streams.filter(resolution="360p", fps=30).first().download(path.dirname(path.realpath(__file__)) + "\\inputs", clean_title, skip_existing=False)
+                yt.streams.filter(resolution="360p", fps=30).first().download(f"{path.dirname(path.realpath(__file__))}\\inputs", clean_title, skip_existing=False)
 
     def __iter__(self):
-        for frame in self.cache:
-            yield frame
+        yield from self.cache
 
         i = 0
         exists = True
@@ -43,3 +43,10 @@ class VideoLoader:
 
     def __len__(self) -> int:
         return int(self.raw_data.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    def __getitem__(self, item: int) -> ndarray:
+        if len(self.cache) <= item:
+            for i, _ in enumerate(self):
+                if i == item:
+                    break
+        return self.cache[item]
--- main.py
+++ main.py
@@ -3,47 +3,65 @@ import cv2
 from tqdm import tqdm
 from VideoLoader import VideoLoader
 
-
-height_resolution = 480  # Lower resolution gives faster results, but might not count every loading frame if the video is highly compressed
+height_resolution = 100  # Lower resolution gives faster results, but might not count every loading frame if the video is highly compressed
 check_each = 2  # Check 1 out of x frames assuming the inaccuracies will cancel out on long runs (2 still gives max precision for 60fps videos since the game runs at 30fps)
+download_quality = {"resolution": "360p", "fps": 30}
 
-pouch_selection_text = "Select the pouch then press enter"
+
+selection_text = lambda element: f"Select the {element} then press enter"
 
 
-def populate_variables(video: VideoLoader):
-    pouch_frame = int(input("Frame where you can see the pouch: "))
-    for i, frame in tqdm(enumerate(video), "Searching first frame", leave=False):
+def select_object(video: VideoLoader, searching: str) -> tuple[int, ...]:
+    pouch_frame = int(input(f"Frame where you can see the {searching}: "))
+    for i, frame in enumerate(video):
         if i != pouch_frame:
             continue
 
-        cv2.namedWindow(pouch_selection_text, 2)
-        roi_480p = cv2.selectROI(pouch_selection_text, cv2.resize(frame, (int(len(frame[0]) * 480 / len(frame)), 480)))
+        cv2.namedWindow(selection_text(searching), 2)
+        roi_480p = cv2.selectROI(selection_text(searching), cv2.resize(frame, (int(len(frame[0]) * 480 / len(frame)), 480)))
         roi = tuple(int(i * height_resolution / 480) for i in roi_480p)
 
-        cv2.destroyWindow(pouch_selection_text)
+        cv2.destroyWindow(selection_text(searching))
+        return roi
 
-        resize_ratio = len(frame) / height_resolution
-        return resize_ratio, roi
+
+def roi_is_black(frame, roi: tuple[int, ...], resize_ratio: float, area: int) -> bool:
+    cropped_frame = cv2.resize(frame, (int(len(frame[0]) / resize_ratio), height_resolution))[
+                    roi[1]:roi[1] + roi[3], roi[0]:roi[0] + roi[2]]
+    black_pixels = 0
+    for pixel in itertools.chain.from_iterable(cropped_frame):
+        if sum(pixel) // 3 < 10:
+            black_pixels += 1
+    if black_pixels / area >= 0.95:
+        return True
 
 
 def count_loading_frames(video: VideoLoader):
     first_frame = int(input("Frame right before the run starts: "))
+    resize_ratio = video.raw_data.get(cv2.CAP_PROP_FRAME_HEIGHT) / height_resolution
 
-    resize_ratio, roi = populate_variables(video)
-    area = roi[2] * roi[3]
+    roi_pouch = select_object(video, "pouch")
+    area_pouch = roi_pouch[2] * roi_pouch[3]
 
-    loading_frames = 0
+    loading_frame_indexes = []
     for i, frame in tqdm(enumerate(video), "Analyzing frame", unit="", colour="#ffffff", total=len(video)):
-        if i <= first_frame or i % 2 != 0:
+        if i <= first_frame or i % check_each != 0:
             continue
 
-        cropped_frame = cv2.resize(frame, (int(len(frame[0]) / resize_ratio), height_resolution))[roi[1]:roi[1] + roi[3], roi[0]:roi[0] + roi[2]]
-        black_pixels = 0
-        for pixel in itertools.chain.from_iterable(cropped_frame):
-            if sum(pixel) // 3 < 10:
-                black_pixels += 1
-        if black_pixels / area >= 0.9:
-            loading_frames += 2
+        if roi_is_black(frame, roi_pouch, resize_ratio, area_pouch):
+            loading_frame_indexes.append(i)
+
+    print(f"Probably a loading screen: {loading_frame_indexes[0] + int(32 * 30 / video.framerate)}")
+    roi_moon = select_object(video, "loading moon")
+    area_moon = roi_moon[2] * roi_moon[3]
+
+    loading_frames = 0
+    for loading_frame_index in tqdm(loading_frame_indexes, "Checking again", unit="", colour="#ffffff", total=len(loading_frame_indexes)):
+        loading_frame = video[loading_frame_index]
+
+        if not roi_is_black(loading_frame, roi_moon, resize_ratio, area_moon):
+            loading_frames += check_each
+
     return loading_frames
 
 
@@ -60,10 +78,12 @@ def seconds_to_string(seconds):
 
 
 def main():
+    recorded_time = float(input("Recorded time: "))
+
     video = VideoLoader(input("Enter a youtube/speedrun.com link or a path to a video file: "))
     seconds_of_loading = count_loading_frames(video) / video.framerate
 
-    print(seconds_to_string(seconds_of_loading))
+    print(seconds_to_string(seconds_of_loading), seconds_to_string(recorded_time - seconds_of_loading))
 
 
 if __name__ == "__main__":
